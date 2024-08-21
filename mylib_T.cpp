6b667005ae2d64a90116393134146878568bc521
RectangleMax <mkubatkin@yandex.ru>
2024-08-21 14:11:08 +0300

    Работа над ошибками (add)

--- mylib_T.cpp
+++
@@ -1,7 +0,0 @@
-template<typename T>
-void output_and_clear_list(std::list<T>& list_of_T) {
-    for (auto it = list_of_T.begin(); it != list_of_T.end(); ++it) {
-        std::cout << *it << std::endl;
-    }
-    list_of_T.clear();
-}
--- mylib_T.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <list>
-#include <string>
-
-template<typename T>
-void output_and_clear_list(std::list<T>&);
-
-#include "mylib_T.cpp"
